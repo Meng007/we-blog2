e3be7e6d7103270d8f9f6ba0aec6bfe3b08523ad
chestnut <352322103@qq.com>
2019-09-30 15:35:10 +0800

    在web-ui新增了test2 test3

--- we-blog-web-ui/src/main/java/test2.java
+++ we-blog-web-ui/src/main/java/test2.java
@@ -2,5 +2,8 @@ public class test2 {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
--- we-blog-web-ui/src/main/java/test3.java
+++ we-blog-web-ui/src/main/java/test3.java
@@ -1,2 +1,8 @@
 public class test3 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
 }
