bc167e3c44284b86e59b8dff93b2e04b7eb0366f
STAN <34086107+sunenn@users.noreply.github.com>
2019-10-10 16:01:12 +0800

    添加了拦截器

--- we-blog-web-admin/src/main/java/we/blog/web/admin/web/Interceptor/LoginInterceptor.java
+++ we-blog-web-admin/src/main/java/we/blog/web/admin/web/Interceptor/LoginInterceptor.java
@@ -8,17 +8,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginInterceptor implements HandlerInterceptor {
+
+    /**
+     * 预处理。controller方法执行前
+     * @param request
+     * @param response
+     * @param o
+     * @return true 放行，false 不放行
+     * @throws Exception
+     */
     @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        Admin admin = (Admin)httpServletRequest.getSession().getAttribute("admin");
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+        Admin admin = (Admin)request.getSession().getAttribute("admin");
         if (admin!=null){
             return true;
         }
         return false;
     }
 
+    /**
+     * 后处理方法，controller执行之后
+     * @param request
+     * @param response
+     * @param o
+     * @param modelAndView
+     * @throws Exception
+     */
     @Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object o, ModelAndView modelAndView) throws Exception {
 
     }
 
